2ca76a04bc1108ea73617aa93bb0da001ea5eb4d
dadaset <89086844+dadaset@users.noreply.github.com>
2022-01-25 17:53:50 -0300

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -62,3 +62,20 @@ print(cars.loc[:,['drives_right']])
 print(cars.loc[:,['cars_per_cap','drives_right']])
 
 
+#######                             LET'S FIND OUT WICH OF THE COUTRIES HAVE MORE THAN 500 CARS PER CAPITA
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+cpc = cars['cars_per_cap']
+many_cars = cpc > 500
+car_maniac = cars[many_cars]
+
+
+# Print car_maniac
+print(car_maniac)
+
+
+
