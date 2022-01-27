c2ce0bbfedd2e0c65f7f758eef336134087958a3
dadaset <89086844+dadaset@users.noreply.github.com>
2022-01-27 16:50:20 -0300

    Update control-flow.py

--- control-flow.py
+++ control-flow.py
@@ -77,8 +77,30 @@ for c,k in europe.items() :
     #the capital of poland is warsaw
     #the capital of austria is vienna
 
+    #___________________________ LOOP OVER A NUMPY ARRAY___________________________________#
+    
+# these two arrays are already loaded
+#   np_height = [74 74 72 ... 75 75 73] 
+#   np_baseball = [[ 74 180]
+# [ 74 215]
+# [ 72 210]
+# ...
+# [ 75 205]
+# [ 75 190]
+# [ 73 195]]
 
+# Import numpy as np
+import numpy as np
 
+# For loop over np_height
+for x in np_height :
+    print(str(x) + " inches")
+
+# For loop over np_baseball
+for x in np.nditer(np_baseball) :
+    print(x)
+    
+#___________________________ LOOP OVER A DATAFRAME___________________________________#
 
 
 
